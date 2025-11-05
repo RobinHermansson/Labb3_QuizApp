dbeaf151af5e9f4b251797d71b2f5a8941d9ff2b
Robin Hermansson <robin.hermansson@stendahls.se>
2025-11-05 05:48:20 +0100

    Addition of QuestionViewModel and some logic

--- Labb3_QuizApp/ViewModels/PlayerViewModel.cs
+++ Labb3_QuizApp/ViewModels/PlayerViewModel.cs
@@ -9,17 +9,46 @@ class PlayerViewModel : ViewModelBase
     public DelegateCommand SetPackNameCommand { get; }
     public QuestionPackViewModel ActivePack { get => _mainWindowViewModel.ActivePack; }
 
+    private int _questionSet = 0;
+
+    private List<QuestionViewModel> _questionViewModels;
+    
+    public QuestionViewModel CurrentQuestion
+    {
+        get
+        {
+            if (_questionViewModels != null && 
+                QuestionSet >= 0 && 
+                QuestionSet < _questionViewModels.Count)
+            {
+                return _questionViewModels[QuestionSet];
+            }
+            return null;
+        }
+    }
+    public int QuestionSet
+    {
+        get => _questionSet;
+        set
+        {
+            _questionSet = value;
+            RaisePropertyChanged();
+        }
+    }
 
     public PlayerViewModel(MainWindowViewModel? mainWindowViewModel)
     {
         _mainWindowViewModel = mainWindowViewModel;
         SetPackNameCommand = new DelegateCommand(SetPackName, CanSetPackName);
         DemoText = string.Empty;
+        PlayGame();
     }
 
     private string _demoText;
-    
-    public string DemoText { get => _demoText;
+
+    public string DemoText
+    {
+        get => _demoText;
         set
         {
             _demoText = value;
@@ -36,5 +65,23 @@ class PlayerViewModel : ViewModelBase
     private void SetPackName(object? obj)
     {
         ActivePack.Name = DemoText;
+    }
+
+    private void NextQuestionSet()
+    {
+        if (ActivePack?.Questions != null && QuestionSet < ActivePack.Questions.Count - 1)
+        {
+            QuestionSet++;
+        }
+    }
+
+    private void PlayGame()
+    {
+        if (ActivePack?.Questions != null)
+        {
+            _questionViewModels = ActivePack.Questions
+                .Select(q => new QuestionViewModel(q))
+                .ToList();
+        }
     }
 }
---
+++ Labb3_QuizApp/ViewModels/QuestionViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Labb3_QuizApp.Models;
+namespace Labb3_QuizApp.ViewModels;
+
+class QuestionViewModel : ViewModelBase
+{
+
+    private Question _question; 
+    public Question Question
+    {
+        get => _question;
+        set
+        {
+            _question = value;
+            RaisePropertyChanged();
+            /*
+            RaisePropertyChanged(nameof(Query));
+            RaisePropertyChanged(nameof(CorrectAnswer));
+            RaisePropertyChanged(nameof(IncorrectAnswers));
+            */
+        }
+    }
+
+    public string Query => _question.Query;
+
+    public string CorrectAnswer => _question.CorrectAnswer;
+
+    public string[] IncorrectAnswers => _question.IncorrectAnswers;
+    public QuestionViewModel(Question model)
+    {
+        _question = model;
+    }
+
+}
