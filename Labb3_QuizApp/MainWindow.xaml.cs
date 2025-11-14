d015fc02259b6135ccc2e212ea5661f37ce8d0b5
Robin Hermansson <robin.hermansson@stendahls.se>
2025-11-14 16:28:40 +0100

    Save packs on closing of the application using an event listener

--- Labb3_QuizApp/MainWindow.xaml.cs
+++ Labb3_QuizApp/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Labb3_QuizApp.ViewModels;
+﻿using Labb3_QuizApp.Services;
+using Labb3_QuizApp.ViewModels;
 using System.Windows;
 namespace Labb3_QuizApp;
 
@@ -11,11 +12,29 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
         DataContext = new MainWindowViewModel();
+        Closing += MainWindow_Closing;
         if (DataContext is MainWindowViewModel viewModel)
         {
             viewModel.SetMainWindow(this);
         }
 
+    }
+    private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+    {
+        if (DataContext is MainWindowViewModel viewModel)
+        {
+            try
+            {
+                var packs = viewModel.Packs.Select(p => p.GetModel()).ToList();
+                var packHandler = new PackHandlerService();
+                packHandler.SaveAllPacks(packs);
+                
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine($"Error saving packs on exit: {ex.Message}");
+            }
+        }
     }
 
 }
