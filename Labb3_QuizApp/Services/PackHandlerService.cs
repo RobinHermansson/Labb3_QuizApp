94013eb1a241e116ae00a5adf123345ca0c37216
Robin Hermansson <robin.hermansson@stendahls.se>
2025-11-14 15:46:57 +0100

    Possibility to load file from localappdata folder from a new download

--- Labb3_QuizApp/Services/PackHandlerService.cs
+++ Labb3_QuizApp/Services/PackHandlerService.cs
@@ -11,21 +11,39 @@ namespace Labb3_QuizApp.Services;
 
 class PackHandlerService
 {
+    private string _appName = "QuizApp_Labb3";
+    private string _questionPacksName = "QuestionPacks.json";
+    private string _localFolder = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+    private string _fullPath;
 
     public PackHandlerService()
     {
-        
+        _fullPath = Path.Combine(_localFolder, _appName);
     }
 
     public List<QuestionPack> LoadAllPacks()
     {
-      return JsonSerializer.Deserialize<List<QuestionPack>>(File.ReadAllText("localPacks.json"));
+        string fullPath = Path.Combine(_localFolder, _appName);
+
+        if (!Directory.Exists(_fullPath))
+        {
+            Directory.CreateDirectory(_fullPath);
+        }
+        if (!File.Exists(Path.Combine(_fullPath, _questionPacksName)))
+        {
+            File.Create(Path.Combine(_fullPath, _questionPacksName));
+            return new List<QuestionPack>() { new QuestionPack("<Default Empty Question Pack>") };
+        }
+        return JsonSerializer.Deserialize<List<QuestionPack>>(File.ReadAllText(Path.Combine(_fullPath,_questionPacksName)));
 
     }
 
     public void SaveAllPacks(List<QuestionPack> qPacks)
     {
-        File.WriteAllText("localPacks.json", JsonSerializer.Serialize(qPacks));
+        if (!Directory.Exists(_fullPath))
+        {
+            Directory.CreateDirectory(_fullPath);
+        }
+        File.WriteAllText(Path.Combine(_fullPath, _questionPacksName), JsonSerializer.Serialize(qPacks));
     }
-
 }
