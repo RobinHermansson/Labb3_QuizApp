2a554b2bf59a56d78b1e282e56556c5f63ef82d3
Robin Hermansson <robin.hermansson@stendahls.se>
2026-01-18 14:42:10 +0100

    Add asynchronous version of DelegateCommand

---
+++ Labb3_QuizApp/Command/AsyncDelegateCommand.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows.Input;
+namespace Labb3_QuizApp.Command;
+
+public class AsyncDelegateCommand : ICommand
+{
+    private readonly Func<object?, Task> _execute;
+    private readonly Func<object?, bool>? _canExecute;
+    private bool _isExecuting;
+
+    public event EventHandler? CanExecuteChanged;
+
+    public AsyncDelegateCommand(Func<object?, Task> execute, Func<object?, bool>? canExecute = null)
+    {
+        ArgumentNullException.ThrowIfNull(execute);
+        _execute = execute;
+        _canExecute = canExecute;
+    }
+
+    public bool CanExecute(object? parameter)
+    {
+        return !_isExecuting && (_canExecute?.Invoke(parameter) ?? true);
+    }
+
+    public async void Execute(object? parameter)
+    {
+        if (!CanExecute(parameter))
+            return;
+
+        try
+        {
+            _isExecuting = true;
+            RaiseCanExecuteChanged();
+            
+            await _execute(parameter);
+        }
+        finally
+        {
+            _isExecuting = false;
+            RaiseCanExecuteChanged();
+        }
+    }
+
+    public void RaiseCanExecuteChanged()
+    {
+        CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+}
